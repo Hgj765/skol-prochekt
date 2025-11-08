c58b7d383e4ba68493e3809bdedc0eef7d846d4a
D_Wickleus <wickle@ug.kth.se>
2025-11-09 00:03:22 +0100

    första med fungerande gui

---
+++ personligt prochekt/Game_of_life.py
@@ -0,0 +1,137 @@
+#daniel wickleus
+#personligt prochekt till dd1310
+
+#kriterier
+#basicly game of life
+#grid med celler
+#en cell överlever om den har 2 eller 3 grannar annars dör den
+#en tom cell som har 3 granar föder en ny cell
+
+
+#VARNING: variabler som heter samma ord kan variera i stavning, jag är dyslektiker. samma variebel har korekt stavning men olika variabler kan variera
+class Cell:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f'({self.x},{self.y})'
+    __repr__ = __str__
+
+
+
+
+
+class Grid:
+    def __init__(self, width, height, cells):
+
+        self.width = width
+        self.height = height
+
+        self.cells = cells
+        self.grann_lista = {}
+
+
+
+
+    def make_grid(self):
+        #skapar en string som ska visa hur griden ser ut, borde mergas med print_grid
+        grid = []
+        for height in range(self.height):
+            grid.append([])
+            for width in range(self.width):
+
+                grid[height].append("-")
+
+        for cell in self.cells:
+            grid[cell.y][cell.x]="*"
+
+
+
+
+
+        return grid
+
+    def print_grid(self):
+        #printar ut en fin grid så att de går att se den
+        for width in range(self.width):
+            print("  ",width, end=" ")
+        print()
+        for i in range(self.width):
+            print(i, self.grid[i])
+
+
+    def grannar(self,cells):
+        #skapar en dict med celler och gämför sen om avståndet till andra celler är 1 aka är ett steg ifrån
+        #metoden måste inte vara i klassen men orkar inte flyta den och de funkar lika bra oavsät
+        granceller ={}
+        for cello in cells:#cello= cell one
+            granceller[cello] = 0
+            for cellt in cells:#cellt = cell two
+                if abs(cello.y - cellt.y) <= 1 and abs(cello.x - cellt.x) <= 1 and(cello.y!=cellt.y or cello.x!=cellt.x) :
+                    granceller[cello]+=1
+
+
+        return(granceller)
+
+        #räkna antalet grannar cellen har
+
+    def födas(self):
+        for cell in self.grann_lista:
+
+                for hight in range(-1,2):
+                    for width in range(-1, 2):
+
+                        if not (hight == 0 and width == 0):#if looparna skulle kunna vara i en med and mellan sig men de blev oläsligt och de här är lite bättre
+                            if not str(Cell(cell.x+width, cell.y+hight)) in list(str(i) for i in self.grann_lista.keys()):
+                                if 0<cell.x+width< self.width and 0<=cell.y+hight< self.height:
+                                    if not str(Cell(cell.x+width, cell.y+hight)) in str(self.cells):
+
+                                        födas_alternativ = self.grannar(set(list(self.grann_lista.keys())+ [Cell(cell.x+width, cell.y+hight)]))
+                                        födas_alternativ_granar = födas_alternativ[next(iter(set(födas_alternativ).difference(set(list(self.grann_lista.keys())))))]
+
+                                        if födas_alternativ_granar ==3:
+                                            self.cells.append(next(iter(set(födas_alternativ).difference(set(list(self.grann_lista.keys()))))))
+
+        print("\n\n\n")
+
+                #döds alternativ tar dubleter och de måste fixas för annars ses sama ruta flera gånger
+
+        # om tom ruta har mer än 3 grannar ska den födas
+
+    def dö(self):
+        for cell in self.grann_lista:
+            if not (2 == self.grann_lista[cell] or self.grann_lista[cell]== 3) or not 0<=cell.x<= self.width or not 0<=cell.y<= self.height:
+                self.cells.remove(cell)
+
+
+
+
+        # om antalet grannar är mindre än mindre än 2 eller mer än 3 ska de dö
+
+    def update(self):
+        print(self.cells)
+        self.grid = self.make_grid()  # inte perfekt sätt att göra det men nu görs de så
+        self.print_grid()
+
+        self.grann_lista = self.grannar(self.cells)
+        self.dö()
+        self.födas()
+
+if __name__ == "__main__":
+    test_grid = Grid(10,
+                10,
+                [
+
+                    Cell(0,0),
+                    Cell(0,1),
+                    Cell(1,0),
+                    Cell(1,1)
+                ]
+                )
+
+
+    for i in range(5):
+        test_grid.update()
+
+
+
--- personligt prochekt/Gui.py
+++ personligt prochekt/Gui.py
@@ -1,23 +1,189 @@
-
-from tkinter import *
-from random import *
-
-window = Tk()
-window.title('Game Of Life')
-
-def create_grid(window):
-    width = 800
-    height = 600
-    canvas = Canvas(window, background='white', width=width, height=height)
+import tkinter as tk
+import json
+from Game_of_life import *
 
 
-    for line in range(0, width, 10): # range(start, stop, step)
-        canvas.create_line([(line, 0), (line, height)], fill='black', tags='grid_line_w')
 
-    for line in range(0, height, 10):
-        canvas.create_line([(0, line), (width, line)], fill='black', tags='grid_line_h')
 
-    canvas.grid(row=0, column=0)
+class App(tk.Tk):
 
-create_grid(window)
-window.mainloop()
+    def __init__(self,width=10,height=10):
+        super().__init__()
+        self.grid_dict = {}
+        self.width = width
+        self.height = height
+
+
+        self.screen_width = self.winfo_screenwidth()#fråga inte ordet var kortare
+        self.screen_height = self.winfo_screenheight()
+
+
+        self.tw = tk.IntVar(value=10)
+        self.th = tk.IntVar(value=10)
+
+
+
+        self.frame1 = tk.LabelFrame(self)
+        self.frame1.grid(row=0,column=0,sticky="NW")
+
+        self.frame2 = tk.LabelFrame(self)
+        self.frame2.grid(row=1, column=0, sticky="Nw")
+
+        self.frame_grid = tk.LabelFrame(self)
+        self.frame_grid.grid(row=0, column=1, sticky="NW", rowspan=2)
+
+
+
+        self.btn = tk.Button(self.frame1, text="Next step", width=16,command=self.next_step)
+        self.btn.grid(row=1, sticky="nw")
+        self.btn = tk.Button(self.frame1, text="Stop", width=16,command=self.stop)
+        self.btn.grid(row=3, sticky="nw")
+
+
+
+
+
+        self.name = tk.Label(self.frame2, text="Bred")
+        self.name.grid(row=0)
+
+        self.name_entry = tk.Entry(self.frame2, textvariable=self.tw)
+        self.name_entry.grid(row=1)
+
+        self.name = tk.Label(self.frame2, text="Höjd")
+        self.name.grid(row=2)
+
+        self.name_entry = tk.Entry(self.frame2, textvariable=self.th)
+        self.name_entry.grid(row=3)
+        self.btn = tk.Button(self.frame2, text="Update grid", width=16, command=self.new_grid)
+        self.btn.grid(row=5, sticky="nw")
+
+
+
+
+        try:
+            with open("file.json", "r") as f:
+                file_c = json.load(f).replace(" ", "").strip("[").strip("]").replace("(", "").replace(")", "").split(",")
+
+                for c in range(0, len(file_c), 2):  # c för cordinate men de blev oläsbart
+
+                    print(int(file_c[c]) > self.width , int(file_c[c + 1]) > self.height)
+
+                    if int(file_c[c]) > self.width:
+                        self.tw.set(int(file_c[c]))
+                        self.new_grid()
+                        print(self.width, self.height)
+
+                    if int(file_c[c + 1]) > self.height:
+                        self.th.set(int(file_c[c + 1]))
+                        self.new_grid()
+                        print(self.width,self.height)
+                    grid.cells.append(Cell(int(file_c[c]), int(file_c[c + 1])))
+                    print(grid.cells)
+
+
+
+
+
+
+        except:
+            with open("file.json", "r") as f:
+                pass
+
+        self.new_grid()
+
+    def stop(self):
+        self.fil(   (str(grid.cells)).strip(" ")     )
+        App.destroy(self)
+
+    def fil(self, x):
+        with open("file.json", "w") as f:
+            json.dump(x, f, indent=2)
+
+
+    def select_cell(self, button):
+        current_color = button.cget("bg")
+
+        vald_cell = Cell(int(button.grid_info()['row']),int(button.grid_info()['column']))
+
+
+        if current_color == "SystemButtonFace":
+            new_color = "green"
+            grid.cells.append(vald_cell)
+
+        else:
+            new_color = "SystemButtonFace"
+
+            for cell in grid.cells:#den ville inte funka annars men de suger
+                if cell.x == vald_cell.x and cell.y == vald_cell.y:
+                    grid.cells.remove(cell)
+        button.config(bg=new_color)
+
+
+    def new_grid(self):
+
+        self.grid_dict = {}
+        # Iterate through every widget inside the frame
+        for widget in self.frame_grid.winfo_children():
+            widget.destroy()
+
+        grid.height  = int(self.th.get())
+        grid.width = int(self.tw.get())
+        self.width = grid.width
+        self.height = grid.height
+
+        self.frame_grid = tk.LabelFrame(self)
+        self.frame_grid.grid(row=0, column=1, sticky="NW", rowspan=2)
+
+        for h in range(int(self.tw.get())):
+            for w in range(int(self.th.get())):
+                self.grid_dict[f"({w},{h})"] = tk.Button(self.frame_grid,
+                                                    text="",
+                                                    width=int((self.screen_width / 30) / self.width),
+                                                    height=int((self.screen_height / 30) / self.height),
+
+                                                    )
+
+                self.grid_dict[f"({w},{h})"].config(command=lambda b=self.grid_dict[f"({w},{h})"]: self.select_cell(b))
+                self.grid_dict[f"({w},{h})"].grid(row=w, column=h, sticky="nw")
+
+        for cell in grid.cells:
+
+            self.grid_dict[str(cell)].config(bg="green")
+
+
+
+    def next_step(self):
+        grid.update()
+
+        for cell in self.grid_dict.keys():
+            self.grid_dict[cell].config(bg="SystemButtonFace")
+
+        for cell in grid.cells:
+
+            self.grid_dict[str(cell)].config(bg="green")
+
+
+
+
+
+grid = Grid(10,
+                10,
+                [
+
+
+                ]
+                )
+
+
+app = App()
+app.mainloop()
+
+
+
+"""
+Cell(0,0),
+                    Cell(0,1),
+                    Cell(1,0),
+                    Cell(1,1)
+                    
+                    """
